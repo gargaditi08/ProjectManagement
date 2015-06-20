6672395701ead2da82271e2ddf0ce09bf564b8af
Kuldeep Saini <gargaditi08@gmail.com>
2015-06-20 13:24:07 +0530

    add color

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -5,7 +5,7 @@
 //  Created by Kuldeep Saini on 20/06/15.
 //  Copyright (c) 2015 Dextrotech Software Private Limited. All rights reserved.
 //
-
+https://github.com/gargaditi08/ProjectManagement.git
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning {
