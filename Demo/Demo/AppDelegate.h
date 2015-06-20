e0fe71466ebe7e70963097549ff244faf45c052c
Kuldeep Saini <gargaditi08@gmail.com>
2015-06-20 13:02:38 +0530

    commit

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Kuldeep Saini on 20/06/15.
+//  Copyright (c) 2015 Dextrotech Software Private Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by Kuldeep Saini on 20/06/15.
+//  Copyright (c) 2015 Dextrotech Software Private Limited. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
